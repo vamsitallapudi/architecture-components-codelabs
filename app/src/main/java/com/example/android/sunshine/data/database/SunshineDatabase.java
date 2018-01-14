93eebf696b8d1d8415276b4cf572016647fc93e1
Vamsi Tallapudi <vamsi@coderefer.com>
2018-01-14 18:41:53 +0530

    8. Create the Database #2

--- app/src/main/java/com/example/android/sunshine/data/database/SunshineDatabase.java
+++ app/src/main/java/com/example/android/sunshine/data/database/SunshineDatabase.java
@@ -5,9 +5,35 @@ package com.example.android.sunshine.data.database;
  */
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverter;
+import android.arch.persistence.room.TypeConverters;
+import android.content.Context;
 
 @Database(entities = {WeatherEntry.class},version = 1)
+@TypeConverters(DateConverter.class)
 public abstract class SunshineDatabase extends RoomDatabase{
+
     public abstract WeatherDao mWeatherDao();
+
+    private static final String DATABASE_NAME = "weather";
+
+//    For Singleton Instantiation
+
+    private static final Object LOCK = new Object();
+    private static volatile SunshineDatabase sInstance;//-> volatile is used for the variables which needs to be stored directly in main memory instead of CPU memory cache(CPU mem cache is separately maintained for different threads hence there might be chance of same variable copy maintaining different values which is harmful to synchronization).
+
+
+    public static SunshineDatabase getInstance(Context context){
+        if(sInstance == null){
+            synchronized (LOCK){
+                if(sInstance == null){
+                    sInstance = Room.databaseBuilder(context.getApplicationContext(),
+                            SunshineDatabase.class,SunshineDatabase.DATABASE_NAME).build();
+                }
+            }
+        }
+        return sInstance;
+    }
 }
